38ad00a68ba106fad1e8a6e501e7245ea406fd23
Ivan Alburquerque <ivan.alburquerque@digitas.com.do>
2020-09-06 13:33:25 -0400

    Finish header implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  purge: ["./public/**/*.html"],
+  purge: ['./public/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '50px': '3.125rem',
+      },
+      maxWidth: {
+        '8xl': '80rem',
+      },
+      minWidth: {
+        'icon': '24px',
+        'icon-md': '40px',
+      },
+      colors: {
+        'dev-blue': {
+          '500': '#3b49df',
+          '700': '#323ebe',
+        },
+      },
+      spacing: {
+        14: '3.5rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
