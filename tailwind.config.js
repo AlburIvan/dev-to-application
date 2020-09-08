d4704cec2c9fd5235aff8e7c2f915fae2b01d6dc
Ivan Alburquerque <ivan.alburquerque@digitas.com.do>
2020-09-07 22:21:34 -0400

    Implement article header & author bottom summary

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,23 @@ module.exports = {
         '50px': '3.125rem',
       },
       maxWidth: {
-        '8xl': '80rem',
+        'site-width': 'var(--site-width)',
       },
       minWidth: {
         'icon': '24px',
         'icon-md': '40px',
+        'icon-lg': '56px',
+        'icon-xl': '64px',
       },
       colors: {
+        'dev-gray': {
+          '500': '#F9FAFA',
+        },
         'dev-blue': {
           '500': '#3b49df',
           '700': '#323ebe',
         },
+        'dev-background': 'var(--site-background)',
       },
       spacing: {
         14: '3.5rem',
